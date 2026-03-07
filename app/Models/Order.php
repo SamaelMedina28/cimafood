429f3cde46838a31540bcb825e6a3b420510094a
SamaelArchPc <samaelortiz2218@gmail.com>
2026-03-06 21:50:15 -0800

    feat: agregar relacion muchos a muchos entre productos y ordenes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // Asignacion masiva
+    protected $fillable = [
+        'user_id',
+        'business_id',
+        'total_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_product')->withPivot('quantity', 'price_unit', 'subtotal');
+    }
 }
