8a122e6c6b97e1e66495f7cbce8e0c65904997d0
SamaelArchPc <samaelortiz2218@gmail.com>
2026-03-07 16:20:24 -0800

    feat: crear vista y ruta de prueba para business

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/business/create', function () {
+        return "<h1 class='text-3xl font-bold underline'>hola</h1>";
+    })->name('business.create');
+
+    Route::get('/business', function () {
+        dd(auth()->user());
+        return "<h1 class='text-3xl font-bold underline'>hola</h1>";
+    })->name('business.index');
 });
