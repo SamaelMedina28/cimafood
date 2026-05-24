f57641a71638c67f2228c1b272a8bbbc6f9fbce6
Samael Medina <samaelortiz2218@gmail.com>
2026-05-24 11:17:04 -0700

    fix: quitar check en carrito cuando se agregaba un producto y cambiarlo por el toast de retroalimentacion
    El check hacia que la pagina de rerenderizara y hubiera saltos en la animacin

--- resources/views/store/business.blade.php
+++ resources/views/store/business.blade.php
@@ -27,15 +27,10 @@
                 }" x-init="init()"
                 @click="window.dispatchEvent(new CustomEvent('open-cart'))">
                 {{-- Ícono del carrito, se convierte en check brevemente al agregar --}}
-                <div x-show="!added" class="text-gray-600">
+                <div class="text-gray-600">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M3 3h2l.4 2M7 13h10l4-4H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                </div>
-                <div x-show="added" x-transition class="text-emerald-600">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M5 13l4 4L19 7" />
                     </svg>
                 </div>
                 {{-- Badge con cantidad --}}
